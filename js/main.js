56fdcd8d584d23f4e3ca9d17ea7092f2546b894b
AzizMakhmudov <azizmakhmudov2412@gmail.com>
2022-10-18 16:12:57 +0500

    commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+var elForm = document.querySelector("#form");
+var elInput = document.querySelector("#input");
+var elSelect = document.querySelector("#select");
+var elHeading = document.querySelector("#heading");
+var piyodaKM = 3.6;
+var velosipedKM = 20.1;
+var mashinaKM = 70;
+var samolyotKM = 800;
+elForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  if (elSelect.value === "piyoda") {
+    elHeading.textContent = `${(elInput.value / piyodaKM).toFixed(2)}`;
+  } else if (elSelect.value === "velosiped") {
+    elHeading.textContent = `${(elInput.value / velosipedKM).toFixed(
+      2
+    )}`;
+  } else if (elSelect.value === "mashina") {
+    elHeading.textContent = `${(elInput.value / mashinaKM).toFixed(2)}`;
+  } else if (elSelect.value === "samolyot") {
+    elHeading.textContent = `${(elInput.value / samolyotKM).toFixed(2)}`;
+  }
+});
